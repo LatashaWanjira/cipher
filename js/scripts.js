518eb5cb2ac91663113a85c7a516364718eeedce
LatashaWanjira <latasha.wanjira@gmail.com>
2016-11-23 11:18:31 +0300

    created cipher js file containing functions

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+var sentence = prompt("Please enter a sentence:");
+var letter = function(firstLetter, lastLetter) {
+    return firstLetter + lastLetter;
+};
+var firstLetter = sentence.charAt(0);
+var lastLetter = sentence.charAt(str.length -1);
+var outputSentence = alert((firstLetter + lastLetter).toUpperCase());
+var reverseletter = function(firstLetter, lastLetter) {
+    return lastLetter + firstLetter;
+};
+var outputSentence2 = alert((lastLetter + firstLetter).toUpperCase());
+var outputSentence3 = function(outputSentence, outputSentence2) {
+    return outputSentence, outputSentence2;
+};
